7d4ddcbe0af465a958010e6c9c357679fd7e6168
anveshdamulur <damulurianvesh2612@gmail.com>
2021-09-24 12:40:27 +0200

    styling components

--- src/Components/Selected_Pokemon.js
+++ src/Components/Selected_Pokemon.js
@@ -5,25 +5,27 @@ const Selected_Pokemon = ({data}) => {
     const [searchInput, setSearchInput] = useState("");    
     return(
         <div className= "pokemon_container">
-            <h1 id="heading">SELECT A POKEMON</h1>
-            <input type="text" id="input_value" name="inpuValue" placeholder="TYPE TO FILTER" onChange={e => {setSearchInput(e.target.value)}} />
-            {
-             data.length && data.filter((info) =>{
-                if(searchInput === ""){
-                    console.log(searchInput)
-                    return info;
+            <div className="pokemon_container_details">
+                <h1 id="heading">SELECT A POKEMON</h1>
+                <input type="text" id="input_value" name="inpuValue" placeholder="TYPE TO FILTER" onChange={e => {setSearchInput(e.target.value)}} />
+                {
+                data.length && data.filter((info) =>{
+                    if(searchInput === ""){
+                        console.log(searchInput)
+                        return info
+                    }
+                    else if (info.name.toLowerCase().includes(searchInput.toLowerCase())){
+                        return info
+                    }
+                }).map(info => {
+                        return (
+                        <div key = {info.id} className="pokemon-list">
+                            <h1 className="list-names">{info.name.toUpperCase()}</h1>
+                        </div>
+                        )
+                    })
                 }
-                else if (info.name.toLowerCase().includes(searchInput.toLowerCase())){
-                    return info
-                }
-             }).map(info => {
-                    return (
-                    <div key = {info.id}>
-                        <h1>{info.name}</h1>
-                    </div>
-                    )
-                 })
-            }
+            </div>
         </div>
     )
 }
